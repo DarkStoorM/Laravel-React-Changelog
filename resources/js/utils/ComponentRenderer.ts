565272af3c51fd9d1b426a9a48730204fa13c88d
DarkStoorM <b.sieprawski@op.pl>
2023-02-18 12:34:37 +0100

    Reword

--- resources/js/utils/ComponentRenderer.ts
+++ resources/js/utils/ComponentRenderer.ts
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom";
 import { selectElementIfExists } from "./ElementSelector";
 
 /**
- * Renders the given React Component if the DOM element with specified ID exists in the DOM
+ * Renders the given React Component if the element with specified ID exists in the DOM
  *
  * @param {JSX.Element}   component React Component to eventually render
  * @param {string}        elementId DOM Element id
