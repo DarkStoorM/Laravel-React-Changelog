c518c0cd9666f725da8dc74dc2d54d6fcab0a013
DarkStoorM <b.sieprawski@op.pl>
2023-02-19 22:43:37 +0100

    Clear Route tests for now

--- tests/Route/RouteTest.php
+++ tests/Route/RouteTest.php
@@ -2,15 +2,8 @@
 
 namespace Tests\Route;
 
-use App\Libs\Utils\NamedRoute;
 use Tests\TestCase;
 
 class RouteTest extends TestCase
 {
-    public function testIsIndexOk()
-    {
-        $response = $this->get(route(NamedRoute::GET_INDEX));
-
-        $response->assertOk();
-    }
 }
