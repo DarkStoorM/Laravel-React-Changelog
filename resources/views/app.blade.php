51707e0b51b841e2e95af2ece0296011b6877735
DarkStoorM <b.sieprawski@op.pl>
2023-03-02 18:30:00 +0100

    Add csrf token for api requests

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,8 @@
     <meta content="width=device-width, initial-scale=1"
           name="viewport">
     <title>{{ config('app.name') }}</title>
-
+    <meta content="{{ csrf_token() }}"
+          name="csrf-token">
     @viteReactRefresh
     @vite(['resources/sass/app.scss', 'resources/js/app.tsx'])
 </head>
