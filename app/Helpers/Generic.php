6e3fa975ce182ddc3d3d7c9a68e0b8d3d45d73a8
DarkStoorM <b.sieprawski@op.pl>
2023-03-02 18:19:29 +0100

    Add a fixed json response for simple messages

--- app/Helpers/Generic.php
+++ app/Helpers/Generic.php
@@ -1,3 +1,18 @@
 <?php
 
 // Placeholder
+
+use Illuminate\Http\JsonResponse;
+
+/**
+ * Returns a fixed JsonResponse for regular message responses, that don't need additional Data
+ *
+ * @param   string  $message  Message received from the server
+ * @param   mixed   $data     Optional data returned by the server
+ */
+function json_response(string $message, $statusCode = 200): JsonResponse
+{
+    $response = json_encode(['status' => $statusCode, 'message' => $message]);
+
+    return response()->json($response, $statusCode);
+}
