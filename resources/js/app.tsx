03194ec5c8131bac334f861042b978b1deb511c0
DarkStoorM <b.sieprawski@op.pl>
2023-03-02 18:29:24 +0100

    Move from Main to App

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,1 +1,25 @@
-import "./Main";
+import "@total-typescript/ts-reset";
+
+import React from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./AppRoutes";
+import { Home } from "./pages/Home";
+import { NewChangelog } from "./pages/NewChangelog";
+import { NotFound } from "./pages/NotFound";
+import { renderIfExists } from "./utils/helpers/ComponentRenderer";
+
+export const token = (document.querySelector('meta[name="csrf-token"]') as HTMLMetaElement).content;
+
+export function Main() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoutes.ROOT} element={<Home />} />
+        <Route path={AppRoutes.CHANGELOG} element={<NewChangelog />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+renderIfExists(<Main />, "app");
