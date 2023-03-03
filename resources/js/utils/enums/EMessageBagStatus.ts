ffbb1bd747431eda8b558b24bc65190c7da3b728
DarkStoorM <b.sieprawski@op.pl>
2023-03-03 01:05:49 +0100

    Extract Message Bag status

---
+++ resources/js/utils/enums/EMessageBagStatus.ts
@@ -0,0 +1,8 @@
+/**
+ * Message Status later applied as a CSS class to the Message Bag component
+ */
+export enum EMessageBagStatus {
+  WARNING = "message-bag--warning",
+  SUCCESS = "message-bag--success",
+  EMPTY = "",
+}
