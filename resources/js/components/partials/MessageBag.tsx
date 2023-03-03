f60e82cb49d39445d758c556bf8829b4c18428b0
DarkStoorM <b.sieprawski@op.pl>
2023-03-03 13:03:25 +0100

    Extract MessageBag to partials

---
+++ resources/js/components/partials/MessageBag.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { IMessageBagProps } from "resources/js/utils/interfaces/IMessageBagProps";
+
+/**
+ * Displays a result message container, having its `background-color` controlled by `status` prop
+ */
+export function MessageBag({ status, messages }: IMessageBagProps) {
+  return (
+    <div className={`message-bag ${status}`}>
+      {messages.map((message) => (
+        <div>{message}</div>
+      ))}
+    </div>
+  );
+}
