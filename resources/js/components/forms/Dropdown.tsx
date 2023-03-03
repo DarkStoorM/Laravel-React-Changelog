bbc9928e5c20ddfdf139873321ca2da5d8fc98d3
DarkStoorM <b.sieprawski@op.pl>
2023-03-03 13:04:26 +0100

    Remove Label

--- resources/js/components/forms/Dropdown.tsx
+++ resources/js/components/forms/Dropdown.tsx
@@ -5,15 +5,13 @@ import { IDropdownProps } from "../../utils/interfaces/IDropdownProps";
  * Creates a Select Element with a dynamically constructed list of options defined in the
  * `props.options: string[]`.
  *
- * If `labeledAs` was passed along with the props, will render a label with the specified text
  * @param props Object of HTMLSelect reference and an array of options
  */
 export function Dropdown(props: IDropdownProps) {
-  const { options, reference, labeledAs } = props;
+  const { options, reference } = props;
 
   return (
     <>
-      {labeledAs && <label htmlFor={labeledAs.elementId}>{labeledAs.text}</label>}
       <select ref={reference} className="input">
         {options.map((option) => (
           <option key={option} value={option}>
