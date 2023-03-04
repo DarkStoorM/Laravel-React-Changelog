1d6c4b76a18dda8c2da8e71c8966b45f0abb34a5
DarkStoorM <b.sieprawski@op.pl>
2023-03-04 19:54:53 +0100

    Regenerate class docs

--- app/Models/Changelog.php
+++ app/Models/Changelog.php
@@ -7,6 +7,26 @@ use App\Transformers\ChangelogTransformer;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Changelog
+ *
+ * @property int $id
+ * @property string $type
+ * @property string $body
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Database\Factories\ChangelogFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Changelog newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Changelog newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Changelog query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Changelog recent()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Changelog whereBody($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Changelog whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Changelog whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Changelog whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Changelog whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Changelog extends Model
 {
     use HasFactory;
