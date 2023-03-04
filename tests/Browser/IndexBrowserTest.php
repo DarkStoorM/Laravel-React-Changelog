fe2fe6de2e7bf01240e2cc20f98c49aa531db1cf
DarkStoorM <b.sieprawski@op.pl>
2023-03-04 20:04:04 +0100

    Add tests

--- tests/Browser/IndexBrowserTest.php
+++ tests/Browser/IndexBrowserTest.php
@@ -2,17 +2,61 @@
 
 namespace Tests\Browser;
 
+use App\Models\Changelog;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
 class IndexBrowserTest extends DuskTestCase
 {
-    public function testExample(): void
+    public function testCanFetchRecentChanges(): void
     {
         $this->browse(function (Browser $browser) {
-            $browser->assertGuest()
-                ->visit('/')
-                ->assertPathIs('/');
+            $browser->visit('/')
+                ->waitUntilMissingText('Fetching changes...')
+                ->assertPresent('#changelog');
+        });
+    }
+
+    public function testCanDisplayEmptyChangelogInfo(): void
+    {
+        Changelog::truncate();
+
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                ->waitUntilMissingText('Fetching changes...')
+                ->assertSeeIn('#uninitialized', 'No changes yet.');
+        });
+    }
+
+    public function testCanCreateNewChangelog(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $changelogBody = 'Changelog body content';
+
+            $browser->visit('/')
+                ->click('#new-entry')
+                ->select('#changelog-type', 'update')
+                ->type('#body', $changelogBody)
+                ->click('#submit-changelog-entry')
+                ->waitFor('#message-bag')
+                ->waitUntilMissingText('Submitting...')
+                ->assertSee('New entry added')
+                // Also check if the same entry appears on the home page without writing a new duplicate test
+                ->click('#home')
+                ->waitUntilMissingText('Fetching changes...')
+                ->assertSee($changelogBody);
+        });
+    }
+
+    public function testGetsStoppedByValidationError(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                ->click('#new-entry')
+                ->click('#submit-changelog-entry')
+                ->waitFor('#message-bag')
+                ->waitUntilMissingText('Submitting...')
+                ->assertSee('The body field is required.');
         });
     }
 }
--- tests/Route/RouteTest.php
+++ tests/Route/RouteTest.php
@@ -2,14 +2,68 @@
 
 namespace Tests\Route;
 
-use App\Models\Changelog;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
 class RouteTest extends TestCase
 {
-    public function testCanFetchRecentChanges(): void
+    use DatabaseMigrations;
+
+    public function testIsRootOk(): void
     {
-        Changelog::factory(10)->create();
         $this->get('/')->assertOk();
+    }
+
+    public function testIsCreateChangelogOk(): void
+    {
+        $this->get('/create')->assertOk();
+    }
+
+    /*
+     * --------------------------------------------------------------------------
+     * REST ROUTES
+     * --------------------------------------------------------------------------
+     */
+
+    public function testCanFetchRootChangelog(): void
+    {
+        // We only care about the server returning OK, so any error should cause a failure
+        // when fetching
+        $this->get('/api/changes')->assertOk();
+    }
+
+    public function testCanCreateChangelog(): void
+    {
+        $this->post('/create', $this->changelog->toArray())
+            ->assertSessionHasNoErrors();
+
+        $this->assertDatabaseHas('changelogs', ['id' => $this->changelog->id]);
+    }
+
+    public function testCanDetectMalformedData(): void
+    {
+        $this->post('/create', [])
+            ->assertSessionHasErrors(['type', 'body']);
+    }
+
+    public function testCanDeleteChangelogEntry(): void
+    {
+        $this->assertDatabaseHas('changelogs', ['id' => $this->changelog->id]);
+
+        $this->delete('/api/changes/' . $this->changelog->id)
+            ->assertOk();
+
+        $this->assertDatabaseMissing('changelogs', ['id' => $this->changelog->id]);
+    }
+
+    /**
+     * Throws once the exception is taken off the Controller
+     *
+     * Remove later (ツ)
+     */
+    public function testRememberToImplementTestsForThisEndpointSmileyFace(): void
+    {
+        $this->get('/api/changes/' . $this->changelog->id)
+            ->assertServerError();
     }
 }
